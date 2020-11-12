ea258398215a43a4c7446e209a6358f74e54b32a
Viktor Gorokhov <patronusv@gmail.com>
2020-11-12 16:06:11 +0200

    добавляет решение 5-го задания

---
+++ js/task-05.js
@@ -0,0 +1,35 @@
+function getAllPropValues (array, prop) {
+  'use strict';
+  // Write code under this line 
+    const newArr = [];
+    
+    for (let i = 0; i < array.length; i += 1){
+        
+        if (prop in array[i]) {
+            newArr.push(array[i][prop]);
+        } else { continue }
+        
+    }
+    return newArr;
+}
+
+// Объекты и ожидаемый результат
+const products = [
+    { name: 'Радар', price: 1300, quantity: 4 },
+    { name: 'Радар', price: 1280, quantity: 2 },
+    { name: 'Радар', price: 1320, quantity: 1 },
+    { name: 'Сканер', price: 2700, quantity: 1 },
+    { name: 'Сканер', price: 2500, quantity: 3 },
+    { name: 'Дроид', price: 400, quantity: 7 },
+    { name: 'Захват', price: 1200, quantity: 2 },
+]; 
+
+
+console.log(getAllPropValues(products, 'name'));
+// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity'));
+// [4, 2, 1, 1, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category'));
+//  []
